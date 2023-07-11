4f22ef4d19d8d41aa3ed9c42567ebd3602545323
Vaginderpal Singh Brar <137271458+Vaginderpal@users.noreply.github.com>
2023-07-11 16:58:53 +0530

    index.js

--- pages/index.js
+++ pages/index.js
@@ -1,162 +1,55 @@
-import {useState, useEffect} from "react";
-import {ethers} from "ethers";
-import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
+const { ethers } = require("ethers");
 
-export default function HomePage() {
-  const [ethWallet, setEthWallet] = useState(undefined);
-  const [account, setAccount] = useState(undefined);
-  const [atm, setATM] = useState(undefined);
-  const [balance, setBalance] = useState(undefined);
-  const [ownerName, setOwnerName] = useState(undefined);
-  const [add, setAdd] = useState(undefined);
-  const [sub, setSub] = useState(undefined);
-  const [mult, setMult] = useState(undefined);
-  const [inputA, setInputA] = useState("");
-  const [inputB, setInputB] = useState("");
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; 
+// Replace with your contract address
 
-  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-  const atmABI = atm_abi.abi;
+// Contract ABI
+const contractABI = [
+  "event Deposit(address indexed owner, uint256 amount)",
+  "event Withdraw(address indexed owner, uint256 amount)",
+  "event Transfer(address indexed from, address indexed to, uint256 amount)",
+  "function deposit(address _account, uint256 _number) public payable",
+  "function withdraw(address _account, uint256 _number) public payable",
+  "function transfer(address _from, address _to, uint256 _number) public payable",
+  "function getBalance(address _address) public view returns(uint256)",
+  "function getOwnerName() public viw returns(string)"
+];
 
-  const getWallet = async() => {
-    if (window.ethereum) {
-      setEthWallet(window.ethereum);
-    }
+const contract = new ethers.Contract(contractAddress, contractABI, provider);
 
-    if (ethWallet) {
-      const account = await ethWallet.request({method: "eth_accounts"});
-      handleAccount(account);
-    }
+// Event listener for Deposit event
+contract.on("Deposit", (owner, amount, event) => {
+    console.log("Deposit event triggered:");
+    console.log("Owner:", owner);
+    console.log("Amount:", amount);
+    console.log("Event:", event);
+});
+
+// Event listener for Withdraw event
+contract.on("Withdraw", (owner, amount, event) => {
+    console.log("Withdraw event triggered:");
+    console.log("Owner:", owner);
+    console.log("Amount:", amount);
+    console.log("Event:", event);
+});
+
+// Event listener for Transfer event
+contract.on("Transfer", (from, to, amount, event) => {
+    console.log("Transfer event triggered:");
+    console.log("From:", from);
+    console.log("To:", to);
+    console.log("Amount:", amount);
+    console.log("Event:", event);
+});
+
+// Get Owner name
+async function getOwnerName() {
+  try {
+    const result = await contract.getOwnerName();
+    console.log("Vaginderpal Singh Brar:", result);
+  } catch (error) {
+    console.error("You are not Owner:", error);
   }
-
-  const handleAccount = (account) => {
-    if (account) {
-      console.log ("Account connected: ", account);
-      setAccount(account);
-    }
-    else {
-      console.log("No account found");
-    }
-  }
-
-  const connectAccount = async() => {
-    if (!ethWallet) {
-      alert('MetaMask wallet is required to connect');
-      return;
-    }
-  
-    const accounts = await ethWallet.request({ method: 'eth_requestAccounts' });
-    handleAccount(accounts);
-    
-    // once wallet is set we can get a reference to our deployed contract
-    getATMContract();
-  };
-
-  const getATMContract = () => {
-    const provider = new ethers.providers.Web3Provider(ethWallet);
-    const signer = provider.getSigner();
-    const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
- 
-    setATM(atmContract);
-  }
-
-  const getBalance = async() => {
-    if (atm) {
-      setBalance((await atm.getBalance()).toNumber());
-    }
-  }
-
-  const deposit = async() => {
-    if (atm) {
-      let tx = await atm.deposit(1);
-      await tx.wait()
-      getBalance();
-    }
-  }
-
-  const withdraw = async() => {
-    if (atm) {
-      let tx = await atm.withdraw(1);
-      await tx.wait()
-      getBalance();
-    }
-  }
-  const checkOwner = async () => {
-    if (atm) {
-      let owner = await atm.checkOwner();
-      setOwnerName("Vaginderpal Singh Brar");
-    }
-  }
-  const addition = async () => {
-      if (atm) {
-        const a = parseInt(inputA);
-        const b = parseInt(inputB);
-        const answer = await atm.addition(a,b);
-        setAdd(answer);
-      }
-  }  
-  const subtraction = async () => {
-    if (atm) {
-      const a = parseInt(inputA);
-      const b = parseInt(inputB);
-      const answer = await atm.substraction(a,b);
-      setSub(answer);
-    }
-  }
-  const multiplication = async () => {
-    if (atm) {
-      const a = parseInt(inputA);
-      const b = parseInt(inputB);
-      const answer = await atm.multiplication(a,b);
-      setMult(answer);
-    }
-  }
-  const handleInputAChange = (event) => {
-    setInputA(event.target.value);
-  };
-
-  const handleInputBChange = (event) => {
-    setInputB(event.target.value);
-  };
-
-
-  const initUser = () => {
-    // Check to see if user has Metamask
-    if (!ethWallet) {
-      return <p>Please install Metamask in order to use this ATM.</p>
-    }
-
-    // Check to see if user is connected. If not, connect to their account
-    if (!account) {
-      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
-    }
-
-    if (balance == undefined) {
-      getBalance();
-    }
-
-    return (
-      <div>
-        <p>Your Account: {account}</p>
-        <p>Your Balance: {balance}</p>
-        <button onClick={deposit}>Deposit 1 ETH</button>
-        <button onClick={withdraw}>Withdraw 1 ETH</button>
-      </div>
-    )
-  }
-  
-
-  useEffect(() => {getWallet();}, []);
-
-  return (
-    <main className="container">
-      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
-      {initUser()}
-      <style jsx>{`
-        .container {
-          text-align: center
-        }
-      `}
-      </style>
-    </main>
-  )
 }
+
+getOwnerName();
