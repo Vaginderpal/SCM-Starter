a9d457e46d794e2a770f67145a3e4cc71f51dffb
Vaginderpal Singh Brar <137271458+Vaginderpal@users.noreply.github.com>
2023-07-15 07:31:32 +0530

    index.js

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,8 @@ const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 const contractABI = [
   "event Deposit(address indexed owner, uint256 amount)",
   "event Withdraw(address indexed owner, uint256 amount)",
-  "event Transfer(address indexed from, address indexed to, uint256 amount)",
   "function deposit(address _account, uint256 _number) public payable",
   "function withdraw(address _account, uint256 _number) public payable",
-  "function transfer(address _from, address _to, uint256 _number) public payable",
   "function getBalance(address _address) public view returns(uint256)",
   "function getOwnerName() public viw returns(string)"
 ];
@@ -33,14 +31,6 @@ contract.on("Withdraw", (owner, amount, event) => {
     console.log("Event:", event);
 });
 
-// Event listener for Transfer event
-contract.on("Transfer", (from, to, amount, event) => {
-    console.log("Transfer event triggered:");
-    console.log("From:", from);
-    console.log("To:", to);
-    console.log("Amount:", amount);
-    console.log("Event:", event);
-});
 
 // Get Owner name
 async function getOwnerName() {
